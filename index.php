778db64988182058d7018c71a37fc10463fe3613
Cseonn9 <37357544+Cseonn9@users.noreply.github.com>
2019-08-08 07:50:54 -0400

    responsive img

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 if($_POST["submit"]) {
     $recipient="seon5.sc@gmail.com";
     $subject="Rental request for Chevrolet Cruze Sedan";
-    $FirstName=$_POST["fname"];
-    $LastName=$_POST["lname"];
     $email=$_POST["email"];
 
     $mailBody="First Name: $fname\n Email: $email\n\n Date in: $datein \n Date Out: $dateout";
